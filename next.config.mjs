e4a9af8d24e8a567c34a523f7cd3c811ea23115e
Martin Jack <134946937+calculatordelusion@users.noreply.github.com>
2025-08-15 19:03:41 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
             },
             {
               protocol: 'https',
-              hostname: 'lxlfwrdbdhafahrrgtzk.supabase.co',
+              hostname: 'txawclvjgysooageecpz.supabase.co',
             },
           ],
     },
